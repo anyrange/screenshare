17f14d3b90cdd312eeddaa3d2f6c371d8e8c5e19
thaseyor <mikeston@mail.ru>
2022-01-17 13:37:32 +0600

    chore: remove server

--- src/peer/index.ts
+++ src/peer/index.ts
@@ -1,12 +1,5 @@
 import Peer from "peerjs";
 
-const key = (import.meta.env.VITE_KEY as string) || "peerjs";
-
 export const createPeer = (id?: string) => {
-  return new Peer(id, {
-    host: "localhost",
-    port: 9000,
-    path: "/peer",
-    key,
-  });
+  return new Peer(id);
 };
